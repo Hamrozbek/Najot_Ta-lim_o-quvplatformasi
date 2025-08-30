2abf3a3c2b7d4b09f2a06bc8281ea7f94a27c9a7
Hamrozbek <hamroznuriddinov41@gmail.com>
2025-08-30 15:32:14 +0500

    statgafdgag git push

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,14 +1,14 @@
-import Vedio from "../../assets/videos/vedio.mp4";
+// import Vedio from "../../assets/videos/vedio.mp4";
 import { LoginForm } from "../../modules";
 
 const Login = () => {
   return (
-    <div className="relative h-[100vh] flex justify-center items-center">
-      <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover -z-10">
+    <div className="h-[100vh] flex justify-center items-center">
+      {/* <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover -z-10">
         <source src={Vedio} type="video/mp4" />
-      </video>
+      </video> */}
 
-      <div className="w-[360px] relative z-10">
+      <div className="w-[360px]">
         <LoginForm />
       </div>
     </div>
